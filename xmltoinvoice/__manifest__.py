32dd1104e17caee6b5074dbe8e2907c437d21e55
José Manuel Fabela <jose.manuel.vilchis@gmail.com>
2019-03-08 11:38:59 -0600

    Addition of UUID field to model and view (Test)

--- xmltoinvoice/__manifest__.py
+++ xmltoinvoice/__manifest__.py
@@ -1,35 +1,37 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "my_module",
+    'name': "XMl to PO/Invoice",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+        Creation of Purchase Orders or Vendor Bill based on XML Data (Mexican CFDi)""",
 
     'description': """
-        Long description of module's purpose
+        Creation of Purchase Orders or Vendor Bill based on XML Data (Mexican CFDi)
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'author': "Manuel Fabela",
+    'website': "https://www.linkedin.com/in/josemanuelvilchis/",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
     # for the full list
-    'category': 'Uncategorized',
-    'version': '0.1',
+    'category': 'Purchases',
+    'version': '0.2',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['base','purchase', 'account_accountant'],
 
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
-        'views/views.xml',
+        'views/purchase_views.xml',
+        'views/account_invoice_view.xml',
         'views/templates.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
         'demo/demo.xml',
     ],
+
+    'application': True,
 }
