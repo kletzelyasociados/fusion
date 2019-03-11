743ed8cdb7f0c9869f395cd60f14d31708bed962
José Manuel Fabela <jose.manuel.vilchis@gmail.com>
2019-03-11 13:45:13 -0600

    Addition of Button that gets the information of the XML and set information to some fields. - Correction of: "xml.parsers.expat.ExpatError: not well-formed (invalid token): line 1, column 9" Error

--- xmltoinvoice/models/account_invoice.py
+++ xmltoinvoice/models/account_invoice.py
@@ -29,27 +29,11 @@ class AccountInvoice(models.Model):
     def generate_record_name(self):
         # Ventana para seleccionar archivo XML
         #filename = askopenfilename()
-        filename = self.x_xml_file.decode('utf-8')
+        decoded = self.x_xml_file.decode('utf-8', errors='ignore')
 
-        print(filename)
+        goodXML = decoded.encode('utf-8')
 
-        # Conversión de archivo a objeto manipulable de python
-        #mydoc = minidom.parse(self.x_xml_file.decode('utf-8'))
-        #mydoc = minidom.parseString(self.x_xml_file.decode('utf-8'))
-
-        RE_XML_ILLEGAL = u'([\u0000-\u0008\u000b-\u000c\u000e-\u001f\ufffe-\uffff])' + \
-                         u'|' + \
-                         u'([%s-%s][^%s-%s])|([^%s-%s][%s-%s])|([%s-%s]$)|(^[%s-%s])' % \
-                         (chr(0xd800), chr(0xdbff), chr(0xdc00), chr(0xdfff),
-                          chr(0xd800), chr(0xdbff), chr(0xdc00), chr(0xdfff),
-                          chr(0xd800), chr(0xdbff), chr(0xdc00), chr(0xdfff))
-        x = re.sub(RE_XML_ILLEGAL, "", self.x_xml_file.decode('utf-8'))
-
-        print(x)
-
-        mydoc = minidom.parseString(x)
-
-        print(mydoc)
+        mydoc = minidom.parseString(goodXML)
 
         # Obtengo el nodo del emisor
         emisor_items = mydoc.getElementsByTagName("cfdi:Emisor")
