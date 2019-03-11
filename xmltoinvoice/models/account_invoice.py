7b42426986909351cbaeafe649501ddeb60a1561
José Manuel Fabela <jose.manuel.vilchis@gmail.com>
2019-03-11 17:46:26 -0600

    Rase Error of Execution of the XML Extractor method without XML file in the Vendor Bill

--- xmltoinvoice/models/account_invoice.py
+++ xmltoinvoice/models/account_invoice.py
@@ -4,6 +4,8 @@ from odoo import models, fields, api
 
 from xml.dom import minidom
 import base64
+from odoo.exceptions import AccessError, UserError, RedirectWarning, ValidationError, Warning
+
 
 # class my_module(models.Model):
 #     _name = 'my_module.my_module'
@@ -26,19 +28,22 @@ class AccountInvoice(models.Model):
     UUID = fields.Text(string='Folio Fiscal')
 
     @api.one
-    def generate_record_name(self):
-        # Ventana para seleccionar archivo XML
-        #filename = askopenfilename()
-        decoded = base64.b64decode(self.x_xml_file)
+    def import_xml_data(self):
 
-        mydoc = minidom.parseString(decoded)
+        if not self.x_xml_file:
+            raise Warning('No hay ningún archivo XML adjunto!')
 
-        # Obtengo el nodo del emisor
-        emisor_items = mydoc.getElementsByTagName("cfdi:Emisor")
+        else:
+            # The file is stored in odoo encoded in base64 bytes column, in order to get the information in the original way
+            # It must have to be decoded in the same base.
+            xml = minidom.parseString(base64.b64decode(self.x_xml_file))
 
-        # Obtengo los datos necesarios
-        NombreEmisor = emisor_items[0].attributes['Nombre'].value
-        RfcEmisor = emisor_items[0].attributes['Rfc'].value
-        RegimenEmisor = emisor_items[0].attributes['RegimenFiscal'].value
+            # Obtengo el nodo del emisor
+            emisor_items = xml.getElementsByTagName("cfdi:Emisor")
 
-        self.write({'UUID': NombreEmisor})
+            # Obtengo los datos necesarios
+            NombreEmisor = emisor_items[0].attributes['Nombre'].value
+            RfcEmisor = emisor_items[0].attributes['Rfc'].value
+            RegimenEmisor = emisor_items[0].attributes['RegimenFiscal'].value
+
+            self.write({'UUID': xml.getElementsByTagName("tfd:TimbreFiscalDigital")[0].attributes['UUID'].value})
