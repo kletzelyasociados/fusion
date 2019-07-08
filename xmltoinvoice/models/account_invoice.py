36cdac5c9484a2c0255a5f10b0b040a604799604
José Manuel Fabela <jose.manuel.vilchis@gmail.com>
2019-07-08 17:07:02 -0500

    Fixing XML Total Amount Format

--- xmltoinvoice/models/account_invoice.py
+++ xmltoinvoice/models/account_invoice.py
@@ -6,7 +6,6 @@ from xml.dom import minidom
 import base64
 from odoo.exceptions import AccessError, UserError, RedirectWarning, ValidationError, Warning
 import re
-import locale
 
 # class my_module(models.Model):
 #     _name = 'my_module.my_module'
@@ -145,12 +144,11 @@ class AccountInvoice(models.Model):
                                     ('company_id', '=', self.company_id.id),
                                     ('commercial_partner_id', '=', self.commercial_partner_id.id),
                                     ('id', '!=', self.id)]):
-                        locale.setlocale(locale.LC_ALL, 'en_US')
                         raise ValidationError("Se ha detectado una referencia de " + NombreEmisor +
                                               " duplicada: " + self.reference +
                                               " timbrada el " + Fecha +
                                               " en el SAT por un monto de " +
-                                              str(locale.currency(Total, grouping=True)))
+                                              '${}'.format(Total))
 
                     #Si tiene lineas de factura
                     if self.invoice_line_ids:
