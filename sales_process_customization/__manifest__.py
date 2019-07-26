f2bb07b121dd46ccb3d1b8aaa93f01a4f55d2361
José Manuel Fabela <jose.manuel.vilchis@gmail.com>
2019-07-26 11:42:20 -0500

    Test of Status modification and addition of payment plan field to model and view

--- sales_process_customization/__manifest__.py
+++ sales_process_customization/__manifest__.py
@@ -1,35 +1,39 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "my_module",
+    'name': "Sales Process Customization",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+        In this module we add the needed functionality for the sales process.""",
 
     'description': """
-        Long description of module's purpose
+        Sale states.
+        Sale approval by manager.
+        New fields to add files that we ask to the customers.
+        Payment plan of customers.
+        Commission calculation of sales team.
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'author': "Manuel Fabela",
+    'website': "https://www.linkedin.com/in/josemanuelvilchis/",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
     # for the full list
-    'category': 'Uncategorized',
-    'version': '0.1',
+    'category': 'Sale',
+    'version': '0.0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['base', 'sale', 'sale_management'],
 
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
+        'views/sale_order_form_customization.xml'
     ],
     # only loaded in demonstration mode
     'demo': [
         'demo/demo.xml',
     ],
+    'installable': True,
+    'auto_install': True,
 }
