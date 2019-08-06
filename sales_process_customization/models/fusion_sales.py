340d86d705d863c59815987af94e55579e994114
José Manuel Fabela <jose.manuel.vilchis@gmail.com>
2019-08-06 16:24:14 -0500

    Adding sale_request button to view

--- sales_process_customization/models/fusion_sales.py
+++ sales_process_customization/models/fusion_sales.py
@@ -412,24 +412,6 @@ class SaleOrder(models.Model):
     @api.multi
     def action_unlock(self):
         self.write({'state': 'sale'})
-
-    @api.multi
-    def action_confirm(self):
-        if self._get_forbidden_state_confirm() & set(self.mapped('state')):
-            raise UserError(_(
-                'It is not allowed to confirm an order in the following states: %s'
-            ) % (', '.join(self._get_forbidden_state_confirm())))
-
-        for order in self.filtered(lambda order: order.partner_id not in order.message_partner_ids):
-            order.message_subscribe([order.partner_id.id])
-        self.write({
-            'state': 'sale',
-            'confirmation_date': fields.Datetime.now()
-        })
-        self._action_confirm()
-        if self.env['ir.config_parameter'].sudo().get_param('sale.auto_done_setting'):
-            self.action_done()
-        return True
     """
 
     @api.multi
