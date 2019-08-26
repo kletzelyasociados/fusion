2151de2fd403068d0d7d88ad0b9f63f9e112644f
ManuelFabela <jose.manuel.vilchis@gmail.com>
2019-08-26 16:21:35 -0500

    Trying to solve ValueError: too many values to unpack (expected 1)

--- invoice_payment_process/models/invoice_payment_check.py
+++ invoice_payment_process/models/invoice_payment_check.py
@@ -304,6 +304,7 @@ class PurchaseOrder(models.Model):
 
     @api.depends('order_line.invoice_lines.invoice_id.state')
     def _compute_paid_total(self):
+
         for order in self:
 
             if order.order_line:
@@ -322,7 +323,7 @@ class PurchaseOrder(models.Model):
 
                             amount_paid = amount_paid + invoice_line.invoice_id.amount_paid_by_line
 
-                self.paid_total = amount_paid
+                order.paid_total = amount_paid
 
 
     paid_total = fields.Float(string='Total Pagado',
