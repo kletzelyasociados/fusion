f8af0afe5c048f023851e64f527e00e3a6a1f812
ManuelFabela <jose.manuel.vilchis@gmail.com>
2019-08-30 13:47:53 -0500

    Adding variation range of 10 cents

--- invoice_payment_process/models/invoice_payment_check.py
+++ invoice_payment_process/models/invoice_payment_check.py
@@ -270,8 +270,10 @@ class AccountInvoice(models.Model):
                                 # Sumar el monto total
                                 inv_total_amount = inv_total_amount + linea_de_factura.price_total
 
+                        difference = purchase_line_total_amount - inv_total_amount - invoice_line.price_total
+
                         # Comparar con el monto de la línea de orden de compra, si es mayor asignar error al arreglo
-                        if inv_total_amount + invoice_line.price_total > purchase_line_total_amount:
+                        if not (-.10 <= difference <= .10):
                             Error.append('\nError en Línea de Factura No. ' + str(i+1) +
                                          ':- Orden de Compra Origen: ' + purchase_line.order_id.name +
                                          '\n********Monto de Línea de Orden de Compra: ' + '${:,.2f}'.format(purchase_line_total_amount) +
