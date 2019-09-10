2f972e6cd544e67d68c79d475e2bfe4d7dcda112
ManuelFabela <jose.manuel.vilchis@gmail.com>
2019-09-09 22:24:35 -0500

    Trying to solve approved_amount when the field is calculating

--- invoice_payment_process/models/invoice_payment_check.py
+++ invoice_payment_process/models/invoice_payment_check.py
@@ -310,9 +310,13 @@ class AccountInvoice(models.Model):
             if Error:
                 raise ValidationError(Error)
 
-    @api.depends('residual')
+    @api.depends('amount_authorized')
     def _compute_authorized_amount(self):
         self.ensure_one()
+        if self.amount_authorized > 0 and self.state == open:
+            self.amount_authorized = 0
+        else:
+            self.amount_authorized = self.amount_authorized
 
 
 class PurchaseOrder(models.Model):
