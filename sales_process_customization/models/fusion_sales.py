7253349e14dd66472dffabc3e13e48edbb662267
ManuelFabela <jose.manuel.vilchis@gmail.com>
2019-11-19 12:39:44 -0600

    Removing code of credit control module that was separated from this one

--- sales_process_customization/models/fusion_sales.py
+++ sales_process_customization/models/fusion_sales.py
@@ -287,38 +287,6 @@ class SaleOrder(models.Model):
 
     labor_voucher_filename = fields.Char(string='Nombre del Archivo')
 
-    # Credit Control Page
-
-    credit_payment_plan_id = fields.One2many('credit.payment.plan',
-                                             'sale_order_id',
-                                             string='Plan de Pagos',
-                                             readonly=True,
-                                             states={'draft': [('readonly', False)]})
-
-    credit_payments_ids = fields.One2many('account.payment',
-                                          'credit_sale_order_id',
-                                          readonly=True,
-                                          string='Pagos Realizados')
-
-    credit_plan_total = fields.Float(string='Total Plan',
-                              store=True,
-                              readonly=True,
-                              digits=(16, 2),
-                              compute='_compute_credit_plan_total')
-
-    credit_paid_total = fields.Float(string='Total Recibido',
-                              store=True,
-                              readonly=True,
-                              digits=(16, 2),
-                              compute='_compute_credit_paid_total')
-
-    credit_open_total = fields.Float(string='Saldo Pendiente Total',
-                              store=True,
-                              readonly=True,
-                              digits=(16, 2),
-                              compute='_compute_credit_open_total')
-
-
     # Other Information Page
 
     sale_date = fields.Date(string='Fecha de Confirmación de Venta',
@@ -410,21 +378,6 @@ class SaleOrder(models.Model):
         if self.env.context.get('mark_so_as_sent'):
             self.filtered(lambda o: o.state == 'leader_approved').with_context(tracking_disable=True).write({'state': 'sent'})
         return super(SaleOrder, self.with_context(mail_post_autofollow=True)).message_post(**kwargs)
-
-    @api.one
-    @api.depends('credit_payment_plan_id.payment_amount')
-    def _compute_credit_plan_total(self):
-        self.credit_plan_total = sum(plan_line.payment_amount for plan_line in self.credit_payment_plan_id)
-
-    @api.one
-    @api.depends('credit_payments_ids.amount')
-    def _compute_credit_paid_total(self):
-        self.credit_paid_total = sum(paid_line.amount for paid_line in self.credit_payments_ids)
-
-    @api.one
-    @api.depends('credit_plan_total', 'credit_paid_total')
-    def _compute_credit_open_total(self):
-        self.credit_open_total = self.credit_plan_total - self.credit_paid_total
 
     @api.multi
     def action_quotation_send(self):
@@ -580,12 +533,6 @@ class account_payment(models.Model):
                                     index=True,
                                     track_visibility='onchange')
 
-    credit_sale_order_id = fields.Many2one('sale.order',
-                                    string='Orden de Venta',
-                                    ondelete='restrict',
-                                    index=True,
-                                    track_visibility='onchange')
-
 
 class Employee(models.Model):
     _inherit = 'hr.employee'
@@ -726,34 +673,3 @@ class Commissions(models.Model):
     @api.multi
     def action_view_invoice_line(self):
         return True
-
-
-class CreditPaymentPlan(models.Model):
-    _name = "credit.payment.plan"
-    _inherit = ['mail.thread', 'mail.activity.mixin', 'portal.mixin']
-    _description = "Credits Payment Plan"
-
-    partner_id = fields.Many2one('res.partner',
-                                 string='Proveedor',
-                                 change_default=True,
-                                 required=True,
-                                 track_visibility='onchange')
-
-    payment_date = fields.Date(string='Fecha de Pago',
-                               required=True,
-                               index=True,
-                               copy=False,
-                               track_visibility='onchange')
-
-    payment_amount = fields.Float(string='Monto',
-                                  required=True,
-                                  digits=(16, 2),
-                                  track_visibility='onchange')
-
-    payment_type = fields.Selection([('terreno', 'Terreno'),('credit', 'Crédito Puente')], string='Tipo de Pago')
-
-    sale_order_id = fields.Many2one('sale.order',
-                                    string='Orden de Venta',
-                                    ondelete='restrict',
-                                    index=True,
-                                    track_visibility='onchange')
