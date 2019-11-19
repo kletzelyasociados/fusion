b031debc38f1ebf8a4be95ed3abf634bc99e006e
ManuelFabela <jose.manuel.vilchis@gmail.com>
2019-11-19 17:27:47 -0600

    First functional version of the module

--- sales_credit_control/__manifest__.py
+++ sales_credit_control/__manifest__.py
@@ -16,7 +16,7 @@
     # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
     # for the full list
     'category': 'Sale',
-    'version': '0.0.1',
+    'version': '1.0.0',
 
     # any module necessary for this one to work correctly
     'depends': ['base', 'sale', 'sale_management', 'sale_stock'],
